cd41ab8dcd3b99fa57e750817c541d3263352207
Sander Perejan <sanderperejan@gmail.com>
2024-11-14 09:46:10 +0700

    Add files via upload

---
+++ TaskManangementSystemGUI/src/main/java/com/mycompany/taskmanangementsystemgui/TaskManangementSystemGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taskmanangementsystemgui;
+
+/**
+ *
+ * @author intel
+ */
+public class TaskManangementSystemGUI {
+
+    public static void main(String[] args) {
+       
+    }
+}
